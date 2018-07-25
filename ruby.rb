ea2766287bb1194f0c0a56c6d1dbd598c0b20f1d
Carlos Perez <solracias@gmail.com>
2018-07-24 20:42:30 -0700

    learned math operators and if statement in ruby

---
+++ ruby.rb
@@ -0,0 +1,38 @@
+puts "this is my first ruby lesson.";
+
+puts "this is my second line.";
+
+print 'test '; # print does not end the line
+
+puts 'no no yes yes' # puts is one line
+
+myCat = 'cute';
+
+puts 'puts ' + myCat;
+
+puts 'print ' + myCat;
+
+# math operators
+puts 6+11;
+puts 20-1;
+puts 8*10;
+puts 100/10;
+puts 10**4; #exponential
+
+# returns what remainder is in the devided number.
+puts 15 % 4; 
+
+
+
+#if, elsif, else, end
+yourAge = 21;
+
+if yourAge <= 17
+  puts 'not old enough for this site'
+elsif yourAge == 21
+  puts 'you can drink'
+elsif yourAge > 100
+  puts "Go to this site: http://snakeoilsiteee.com"
+else
+  puts 'what is your gender?'
+end
